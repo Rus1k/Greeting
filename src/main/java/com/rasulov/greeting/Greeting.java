b50e089ae8060e7a0beff0dc03c0eae5df87413d
Rus1k <rfrasulov@gmail.com>
2015-12-20 19:14:26 +0200

    first commit

---
+++ src/main/java/com/rasulov/greeting/Greeting.java
@@ -0,0 +1,38 @@
+package com.rasulov.greeting;
+
+import java.time.LocalTime;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Greeting {
+    public static final String MORNING = "my.morning";
+    public static final String DAY = "my.day";
+    public static final String EVENING = "my.evening";
+    public static final String NIGHT = "my.night";
+
+    public static void greetingService() {
+        Locale defaultLocale = Locale.getDefault();
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("label", defaultLocale);
+        displayValues(resourceBundle);
+    }
+
+    public static void displayValues(ResourceBundle bundle) {
+
+        LocalTime time = LocalTime.now();
+        int hour = time.getHour();
+
+        if (hour >= 6 && hour < 9) {
+            System.out.println(bundle.getString(MORNING));
+        }
+        if (hour >= 9 && hour < 19) {
+            System.out.println(bundle.getString(DAY));
+        }
+        if (hour >= 19 && hour < 23) {
+            System.out.println(bundle.getString(EVENING));
+        }
+        if (hour < 6 || hour == 23) {
+            System.out.println(bundle.getString(NIGHT));
+        }
+    }
+}
+
